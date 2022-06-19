8a615261b99b17abd6d65a08fe22b5c7bdf64038
Bre98 <sansomfaith@gmail.com>
2022-06-19 11:35:54 -0500

    New programs

---
+++ favorite_number.py
@@ -0,0 +1,5 @@
+# June 19 2022
+# printing out my favorite number
+
+fav_num = 10
+print(f'My favorite number is {fav_num}')
---
+++ number_eight.py
@@ -0,0 +1,11 @@
+# adding numbers to total eight
+print(5+3)
+
+# subtracting numbers to total eight
+print(10-2)
+
+# multiplying numbers to total eight
+print(4*2)
+
+# dividing numbers to total eight
+print(16/2)
